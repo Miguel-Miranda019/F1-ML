9f1f3eb0a6e3157e46b82a2eb95dce2c875b2ec5
Miguel-Miranda019 <mmiranda2003@gmail.com>
2025-07-22 21:09:11 +0100

    rag test
    testing rag

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-def main():
-    print("Hello from f1-ml!")
+from langchain_ollama.llms import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+
+model = OllamaLLM(model="llama3.1")
+
+template = """
+You are a Pokemon expert. You know everything about Pokemon, including their types, abilities, and evolutions.
+You will be given a question about Pokemon, and you should answer it in detail.
+
+Here is some relevant pokemon info: {pokeInfo}
+
+Here is the question to answer: {question}
+"""
+
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
 
 
-if __name__ == "__main__":
-    main()
+while true:
+    print("\n\n-------------------------------------------------------")
+    question = input("Ask your question (q to quit): ")
+    print("\n\n")
+    if question == "q":
+        break
+    result = chain.invoke({"pokeInfo": [], "question": question})
+    print(result)
